1814827ae83d3d78ca93286c23f66ad72200a37c
Quinn Samms <qsamms11@gmail.com>
2022-06-24 18:52:45 -0700

    grabbing link from iframe some of the time, need to handle exception, skill matcher is working though and the rest of output

--- indeedscraper.py
+++ indeedscraper.py
@@ -4,6 +4,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 import time
 
 print("Please only enter 1 space between words")
@@ -13,6 +14,25 @@ fixedjob = stripped.replace(" ","%20")
 location = input("What location?")
 stripped = location.strip()
 fixedlocation = stripped.replace(" ","%20")
+print("Enter your qualifications, and keywords you would like to see in a workplace")
+print("To help better match you with a company, type 'done' when done")
+
+stop = False
+keywords = []
+fixedwords = []
+while not stop:
+    temp = input()
+    if(temp == 'done'):
+        break
+    keywords.append(temp)
+    
+for word in keywords:
+    one = str(word).strip()
+    two = one.replace(" ","")
+    newword = two.lower()
+    fixedwords.append(newword)
+
+print(fixedwords)
 
 driver = webdriver.Chrome()
 indeedURL = "https://www.indeed.com/jobs?q="+fixedjob+"&l="+fixedlocation
@@ -70,24 +90,36 @@ for i in range(len(infos)):
     print(titles[i].text)
     print(justname)
     print(location)
-
     if(correctrating == ""):
         correctrating = "N/A"
-    print("Company rating: " + correctrating)
-    print()
+    print(f"Compant rating: {correctrating}")
 
-    time.sleep(2)
-    iframe = driver.find_element(By.TAG_NAME,"iframe")
-    driver.switch_to.frame(iframe)
+    button = WebDriverWait(driver,30).until(EC.frame_to_be_available_and_switch_to_it((By.TAG_NAME,"iframe")))
+    try:
+        linkcontainer = driver.find_element(By.XPATH,'//div[@id="viewJobButtonLinkContainer"]')
+        link = linkcontainer.find_element(By.TAG_NAME,'a')
+        linkdisplay = link.get_attribute("href")
+    except nosuchelementexcpetion:
+        
     description = driver.find_element(By.XPATH,'//div[@class="jobsearch-jobDescriptionText"]').text
-    print(description)
     driver.switch_to.parent_frame()
-    if(i == len(infos) - 1):
-        quit()
-    time.sleep(2)
-    titles[i+1].click()
-    time.sleep(2)
-    print()
+
+    if(i != len(infos) - 1):
+        element2 = WebDriverWait(driver,30).until(EC.element_to_be_clickable((titles[i+1])))
+        titles[i+1].click()
+
+    wholething = titles[i].text + justname + location + correctrating + description
+
+    wholethingstripped = str(wholething).strip()
+    wholethingwithoutspaces = wholethingstripped.replace(" ","")
+    fixedwhole = wholethingwithoutspaces.lower()
+
+    print('Your Skills/Needs that match with this Company:')
+    for word in fixedwords:
+        if word in fixedwhole:
+            print(word + " ")
+    print(f"Apply here: {linkdisplay}")
+    print('\n\n')
 
 time.sleep(10000000)
 driver.quit()
